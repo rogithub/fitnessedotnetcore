faf4a641ddf95b190c48b89ebc009a6617cf8dcb
rodrigo debian <correo.rodrigo@gmail.com>
2019-01-26 14:18:15 -0600

    progress

---
+++ fitnesse/startcommand.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "==================CONFIG INFO================================"
+echo "https://fitsharp.github.io/FitSharp/RunTestsWithFitnesse.html"
+echo "============================================================="
+java -jar fitnesse-standalone.jar -p 8080
+exit 0
